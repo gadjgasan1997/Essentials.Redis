260c07104a68ac3d018a880fc8da1f75ccb5a824
gadjgasan1997 <gadj.gasan1997@gmail.com>
2024-08-31 22:55:51 +0300

    Реализовали пакет с утилитами для написания тестов

---
+++ src/Essentials.Redis.TestsUtils/Extensions/RedisCacheServiceExtensions.cs
@@ -0,0 +1,113 @@
+﻿using Moq;
+using LanguageExt;
+using StackExchange.Redis;
+
+namespace Essentials.Redis.TestsUtils.Extensions;
+
+/// <summary>
+/// Методы расширения для <see cref="IRedisCacheService" />
+/// </summary>
+public static class RedisCacheServiceExtensions
+{
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRedisCacheService.HashSetAsync(string, HashSetRecord, When)" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> Setup_HashSetAsync(
+        this Mock<IRedisCacheService> mock,
+        Func<string, HashSetRecord, When, TryAsync<bool>> func)
+    {
+        mock
+            .Setup(service =>
+                service.HashSetAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<HashSetRecord>(),
+                    It.IsAny<When>()))
+            .Returns(func);
+        
+        return mock;
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRedisCacheService.HashSetAsync(string, IEnumerable{HashSetRecord}, When)" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> Setup_HashSetAsync(
+        this Mock<IRedisCacheService> mock,
+        Func<string, IEnumerable<HashSetRecord>, When, TryAsync<Unit>> func)
+    {
+        mock
+            .Setup(service =>
+                service.HashSetAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<IEnumerable<HashSetRecord>>(),
+                    It.IsAny<When>()))
+            .Returns(func);
+        
+        return mock;
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRedisCacheService.HashDeleteAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> Setup_HashDeleteAsync(
+        this Mock<IRedisCacheService> mock,
+        Func<string, string, TryAsync<Unit>> func)
+    {
+        mock
+            .Setup(service =>
+                service.HashDeleteAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+            .Returns(func);
+        
+        return mock;
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRedisCacheService.HashGetAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> Setup_HashGetAsync(
+        this Mock<IRedisCacheService> mock,
+        Func<string, string, TryOptionAsync<HashSetRecord>> func)
+    {
+        mock
+            .Setup(service =>
+                service.HashGetAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+            .Returns(func);
+        
+        return mock;
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRedisCacheService.HashPopAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> Setup_HashPopAsync(
+        this Mock<IRedisCacheService> mock,
+        Func<string, string, TryOptionAsync<HashSetRecord>> func)
+    {
+        mock
+            .Setup(service =>
+                service.HashPopAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+            .Returns(func);
+        
+        return mock;
+    }
+}
---
+++ src/Essentials.Redis.TestsUtils/Extensions/RedisCacheServiceFailResponseExtensions.cs
@@ -0,0 +1,95 @@
+﻿using Moq;
+using LanguageExt;
+using LanguageExt.Common;
+using StackExchange.Redis;
+using static LanguageExt.Prelude;
+
+namespace Essentials.Redis.TestsUtils.Extensions;
+
+/// <summary>
+/// Методы расширения для <see cref="IRedisCacheService" />, возвращающие ошибочный ответ
+/// </summary>
+public static class RedisCacheServiceFailResponseExtensions
+{
+    private static readonly Error _unknownError = new Exception("Unknown error");
+
+    /// <summary>
+    /// Устанавливает для мока возврат ошибки из метода <see cref="IRedisCacheService.HashSetAsync(string, HashSetRecord, When)" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> SetupFail_HashSetItemAsync(
+        this Mock<IRedisCacheService> mock,
+        Error? error = null)
+    {
+        return mock.Setup_HashSetAsync((string _, HashSetRecord _, When _) =>
+        {
+            return TryAsync<bool>(() => throw (error ?? _unknownError).ToErrorException());
+        });
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока возврат ошибки из метода <see cref="IRedisCacheService.HashSetAsync(string, IEnumerable{HashSetRecord}, When)" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> SetupFail_HashSetListAsync(
+        this Mock<IRedisCacheService> mock,
+        Error? error = null)
+    {
+        return mock.Setup_HashSetAsync((string _, IEnumerable<HashSetRecord> _, When _) =>
+        {
+            return TryAsync<Unit>(() => throw (error ?? _unknownError).ToErrorException());
+        });
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока возврат ошибки из метода <see cref="IRedisCacheService.HashDeleteAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> SetupFail_HashDeleteAsync(
+        this Mock<IRedisCacheService> mock,
+        Error? error = null)
+    {
+        return mock.Setup_HashDeleteAsync((_, _) =>
+        {
+            return TryAsync<Unit>(() => throw (error ?? _unknownError).ToErrorException());
+        });
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока возврат ошибки из метода <see cref="IRedisCacheService.HashGetAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> SetupFail_HashGetAsync(
+        this Mock<IRedisCacheService> mock,
+        Error? error = null)
+    {
+        return mock.Setup_HashGetAsync((_, _) =>
+        {
+            return TryOptionAsync<HashSetRecord>(() => throw (error ?? _unknownError).ToErrorException());
+        });
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока возврат ошибки из метода <see cref="IRedisCacheService.HashPopAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRedisCacheService> SetupFail_HashPopAsync(
+        this Mock<IRedisCacheService> mock,
+        Error? error = null)
+    {
+        return mock.Setup_HashPopAsync((_, _) =>
+        {
+            return TryOptionAsync<HashSetRecord>(() => throw (error ?? _unknownError).ToErrorException());
+        });
+    }
+}
