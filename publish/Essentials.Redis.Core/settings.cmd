d857b085d4a47cfe126c56c45a39fa0053b2c09d
gadjgasan1997 <gadj.gasan1997@gmail.com>
2024-08-31 21:24:35 +0300

    Первоначальная реализация репозитория

---
+++ publish/Essentials.Redis.Core/settings.cmd
@@ -0,0 +1,4 @@
+set projectName=Essentials.Redis.Core
+set packageName=Essentials.Redis
+set packageVersion=1.0.0
+set configuration=Release
---
+++ publish/Essentials.Redis.TestsUtils/settings.cmd
@@ -0,0 +1,4 @@
+set projectName=Essentials.Redis.TestsUtils
+set packageName=Essentials.Redis.TestsUtils
+set packageVersion=1.0.0
+set configuration=Release
